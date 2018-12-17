2d12e4ec142c53b0eabba3492bcaf927122e3330
Clark Newell <williamcnewell@gmail.com>
2018-12-17 00:36:14 -0700

    updated FindHighestInterval

--- FindHighestInterval.js
+++ FindHighestInterval.js
@@ -2,13 +2,32 @@
 // Write a function that finds the highest interval between integers in an integersay
 // The integers may be positive or negative
 
+// let integers = [1, 5, -12, -2, 12]
+
+// Result should be 24
+
+// let integers = [5, -25, 15, 1]
+
+// Result should be 40
+
+// let integers = [6, 12, -2, 20]
+
+// Result should be 22
+
 let integers = [1, 5, -12, -2, 12]
 
-function findHighestInterval (integers) {
-    for ( i=0; i < integers.length; i++ ) {
-        if 
+// Result should be 24
+
+const highestInterval = integers => {
+    let highestInt = null;
+    for (let x = 0; x < integers.length; x++) {
+        for (let y = x + 1; y < integers.length; y++) {
+            if (integers[x] < integers[y] && highestInt < (integers[y] - integers[x])) {
+                highestInt = integers[y] - integers[x]
+            }
+        }
     }
-    return result
+    return highestInt === null ? -1 : highestInt;
 }
 
-console.log(findHighestInterval(integers))
+console.log(highestInterval(integers))
